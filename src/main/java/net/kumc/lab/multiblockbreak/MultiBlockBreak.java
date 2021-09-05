20add588b57702cdbade86697b98a23f127cda9e
maru-256 <maru043@docomo.ne.jp>
2021-09-06 03:33:50 +0900

    initial commit

---
+++ src/main/java/net/kumc/lab/multiblockbreak/MultiBlockBreak.java
@@ -0,0 +1,14 @@
+package net.kumc.lab.multiblockbreak;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MultiBlockBreak extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
